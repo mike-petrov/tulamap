1898337204ebc419918f592a0aa2fce6d20f32ad
mike-petrov <mis5116@yandex.ru>
2021-02-21 22:11:11 +0300

    fix

--- script.js
+++ script.js
@@ -11,8 +11,6 @@ function init() {
     });
 
     for (let i = 0; i < roads.length; i += 1) {
-        // coordinates district road date_final_repair long name
-        // Название,Протяжённость,Фактическая дата окончания ремонта дороги,Дорога,Район,Координаты
         let mapPoint = new ymaps.Placemark(JSON.parse(roads[i].coordinates)[0][0], {
             balloonContent: `<h5>${roads[i].name}</h5>Протяжённость: ${roads[i].long}<br>Фактическая дата окончания ремонта дороги: ${roads[i].date_final_repair}<br>Дорога: ${roads[i].road}<br>Район: ${roads[i].district}`,
             hintContent: roads[i].name,
@@ -40,7 +38,6 @@ function init() {
                     .add(mapLine)
             })
             .add('mouseleave', function(e) {
-              console.log(myMap.geoObjects);
                 myMap.geoObjects
                     .remove(mapLine)
             });
